25e542347d653bb03a7974cac06aee72c60d3e6a
RMelanson <remcogold@gmail.com>
2018-05-03 02:18:25 -0400

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -1,1 +1,7 @@
-TaDa
+
+#Install nodejs
+nodejsPkg=sudo yum install nodejs --enablerepo=epel-testing
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/$nodejsPkg
+sudo rpm -ivh $nodejsPkg
+sudo yum install nodejs --enablerepo=epel-testing
+rm $nodejsPkg
