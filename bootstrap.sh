8746d245f79aaea5c167b3b40fa796e05a3ab3c0
RMelanson <remcogold@gmail.com>
2018-05-03 01:48:30 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-yum groupinstall "Development Tools" -y
-yum install curl-devel -y
-yum install fuse-devel -y
-yum install libxml2-devel -y
-yum install openssl-devel -y
-# OPTIONAL REPOSITORIES
-#yum install git -y
-yum install svn
-
-#Install nodejs
-nodejsPkg=sudo yum install nodejs --enablerepo=epel-testing
-wget http://download.fedoraproject.org/pub/epel/6/x86_64/$nodejsPkg
-sudo rpm -ivh $nodejsPkg
-sudo yum install nodejs --enablerepo=epel-testing
-rm $nodejsPkg
+currDir=$PWD
+devToolsDir=/tmp/scripts/devtools
+git clone https://github.com/RMelanson/linux-scripts-devtools.git $devToolsDir
+cd $devToolsDir
+chmod 744 setup.sh
+./setup.sh
+cd $currDir
