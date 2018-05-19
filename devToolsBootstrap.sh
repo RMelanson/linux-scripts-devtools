8d55139dfba2e7217373ad590629feadfd6d777e
Robin Melanson <remcogold@gmail.com>
2018-05-19 13:07:06 -0400

    Create devToolsBootstrap.sh

---
+++ devToolsBootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+devToolsCurrDir=$PWD
+
+#Set Cloning Properties
+pkg=devTools
+gitRepo="linux-scripts-devtools.git"
+installDir="/tmp/scripts/devtools"
+if [ "$1" = "ssh" ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $devToolsCurrDir
