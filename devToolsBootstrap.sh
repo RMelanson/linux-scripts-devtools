f7674c8ee71a9eacb060e31f57c2f973f54ccf1b
Robin Melanson <remcogold@gmail.com>
2018-05-24 10:38:45 -0400

    Update devToolsBootstrap.sh

--- devToolsBootstrap.sh
+++ devToolsBootstrap.sh
@@ -5,7 +5,7 @@ devToolsCurrDir=$PWD
 pkg=devTools
 gitRepo="linux-scripts-devtools.git"
 installDir="/tmp/scripts/dev/tools"
-if [ -f "~/.ssh/gitHub.key" ]; then
+if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone git@github.com:RMelanson/"
 else
    clone="git clone https://github.com/RMelanson/"
