84d3d4fcc1758e66d0b9d0e6aade226f965ddfe4
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:16:40 -0400

    Update devToolsBootstrap.sh

--- devToolsBootstrap.sh
+++ devToolsBootstrap.sh
@@ -14,8 +14,6 @@ yum install git -y
 # SETUP ENVIRONMENT AND PARAMETERS
 . ./env/setEnv.sh
 
-gitRepo="linux-scripts-devtools.git"
-installDir="/tmp/scripts/dev/tools"
 if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone git@github.com:RMelanson/"
 else
