e6470fc64c75da1f0f101f0520baaa30639737c0
Robin Melanson <remcogold@gmail.com>
2019-07-03 12:45:34 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -10,4 +10,8 @@ pkg=DEV_TOOLS
 scriptType="dev"
 parentDir="/tmp/scripts/$scriptType/"
 installDir="$parentDir/$pkg"
-=/opt/mvn
+
+# MAVEN INSTALATION PARMS
+MVN_HOME=/opt/mvn
+mvnURL=http://apache.mirror.iweb.ca/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+
