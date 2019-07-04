d8a1feb6201d6cc4ea85bc3afceb7622b26495e3
Robin Melanson <remcogold@gmail.com>
2019-07-04 02:56:32 -0400

    Update installMaven.sh

--- installs/installMaven.sh
+++ installs/installMaven.sh
@@ -1,5 +1,13 @@
- mkdir $M2_HOME
- echo installMaven EXECUTING: wget $mvnURL
- wget $mvnURL
- echo installMaven EXECUTING: tar -xvzf $mvnVersion
- tar -xvzf $mvnVersion
+currDir=$PWD
+mkdir $M2_HOME
+cd $M2_HOME
+echo installMaven EXECUTING: wget $mvnURL
+wget $mvnURL
+echo installMaven EXECUTING: tar -xvzf $mvnZippedFile
+tar -xvzf $mvnVersion
+echo "M2_HOME=$M2_HOME" >  $mvn_CI_EnvFile
+echo "M2=$M2" >>  $mvn_CI_EnvFile
+echo "PATH=$PATH:
+echo ". $mvn_CI_EnvFile" >> /etc/bashrc
+
+cd $currDir
